e393e449f51b2074d6a88c214664e961346a1945
Eric Julià <59866781+erju-dev@users.noreply.github.com>
2024-02-12 20:07:58 +0100

    Update api_get_dict.py

--- api_get_dict.py
+++ api_get_dict.py
@@ -8,26 +8,26 @@ provincies = requests.get(url)
 print("status code: ", provincies.status_code, "\n")
 #print (provincies.headers)
 #print (provincies.text)
-provincies_dict = provincies.json()
-#print(type(provincies_dict))
 
-for key1 in provincies_dict.keys():
+prov = provincies.json()
+#print(type(prov)) #dict
+
+for key1 in prov.keys():
 	print("{{", key1, "}}")
-	if "dict" in str(type(provincies_dict[key1])):
-		for key2 in provincies_dict[key1].keys():
+	if "dict" in str(type(prov[key1])):
+		for key2 in prov[key1].keys():
 			print("   ◻️", key2, ": ", end="")
-			print(provincies_dict[key1][key2])
-	elif "list" in str(type(provincies_dict[key1])):
-		for num, (info) in enumerate(provincies_dict[key1]):
+			print(prov[key1][key2])
+	elif "list" in str(type(prov[key1])):
+		for num, (info) in enumerate(prov[key1]):
 			print(f"   {num})")
 			for key, value in info.items():
 				print("   ◻️", key, ":", value)
 			print()
 	else:
-		print("   ◻️", provincies_dict[key1])
+		print("   ◻️", prov[key1])
 	print()
 	
-print()
 print()
 
 temps = requests.get("https://www.el-tiempo.net/api/json/v2/provincias/08", timeout=5)
